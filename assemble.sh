8e9279c64ab5f2e3be73cead1e5c18ecb3c5decb
Sasha Finkelstein <fnkl.kernel@gmail.com>
2024-11-15 09:38:58 +0100

    Update to 20241114, prune rootfs more.

--- assemble.sh
+++ assemble.sh
@@ -1,13 +1,44 @@
 #!/bin/bash
 set -euo pipefail
-mkdir -p rootfs chroot/{etc,dev,proc,sys} chroot/var/{cache,lib,lib/dbus}/
+mkdir -p rootfs chroot/{etc,dev,proc,sys,usr/{bin,lib,share}}
 cd rootfs
 tar xf ../stage4-amd64-desktop-systemd.tar.xz
 rm -r sys dev proc
-mv etc/{hosts,resolv.conf,localtime,passwd,group,{,g}shadow,fstab,sub{u,g}id} ../chroot/etc
-mv boot home media mnt root tmp run ../chroot/
-mv var/{tmp,run,lock} ../chroot/var/
-mv var/lib/dbus/machine-id ../chroot/var/lib/dbus/
+find etc/ -mindepth 1 -maxdepth 1 \
+     \! -name eselect -a \
+     \! -name 'ld.so*' \
+     -exec mv {} ../chroot/etc/ \;
+mv boot home media mnt opt root run tmp var ../chroot/
+mv usr/{include,libexec,local,sbin,src,x86_64-pc-linux-gnu} ../chroot/usr/
+mv usr/lib/mingw64-toolchain ../chroot/usr/lib/
+find usr/bin -mindepth 1 -maxdepth 1 \
+     \! -name 'wine*' -a \
+     \! -name 'mango*' -a \
+     \! -name 'notepad*' -a \
+     \! -name 'msi*' -a \
+     \! -name 'regedit*' -a \
+     \! -name 'regsvr32*' -a \
+     \! -name '*vulkan*' -a \
+     \! -name '*vk*' -a \
+     \! -name ulimit -a \
+     \! -name ldd -a \
+     \! -name env -a \
+     \! -name sh -a \
+     \! -name bash -a \
+     \! -name ls -a \
+     \! -name stat -a \
+     \! -name dirname -a \
+     \! -name realpath -a \
+     \! -name basename -a \
+     \! -name nproc -a \
+     \! -name uname -a \
+     \! -name arch -a \
+     -exec mv {} ../chroot/usr/bin/ \;
+find usr/share -mindepth 1 -maxdepth 1 \
+     \! -name 'wine*' -a \
+     \! -name drirc.d -a \
+     \! -name vulkan \
+     -exec mv {} ../chroot/usr/share/ \;
 cd ..
 mksquashfs rootfs fex-rootfs.sqfs -comp zstd
 mksquashfs chroot fex-chroot.sqfs -comp zstd
